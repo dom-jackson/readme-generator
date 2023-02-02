a0013ea5b77110904708df80d187fcc644b5e2d0
TheBestByte <dom_jackson_92@hotmail.com>
2023-02-02 14:14:30 +1100

    added the html literal under generate markdown function. render badge/license/section functions completed

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,95 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "APACHE 2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "GPL 3.0":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    case "BSD 3":
+      return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+    default:
+      return "";
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "APACHE 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "GPL 3.0":
+      return "https://www.gnu.org/licenses/gpl-3.0";
+    case "BSD 3":
+      return "https://opensource.org/licenses/BSD-3-Clause";
+    default:
+      return "";
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+  return `## License
+This project is licensed under the ${license} license.
+See the [LICENSE](${renderLicenseLink(license)}) file for more information.`;
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.project}
+  ${renderLicenseBadge(data.license)}
 
-`;
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Contribution](#contribution)
+  
+  ## Installation
+  
+  To install necessary dependancies, run the following command:
+  
+  \`\`\`
+  ${data.dependancies}
+  \`\`\`
+  
+  ## Usage
+  
+  ${data.repo}
+  
+  ${renderLicenseSection(data.license)}
+  
+  ## Tests
+  
+  To run tests, run the following command:
+  
+  \`\`\`
+  ${data.tests}
+  \`\`\`
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact me directly on ${data.email}. You can find more of my work at [${data.username}](https://github.com/${data.username}/).
+  
+  ## Contribution
+  
+  ${data.contributors}`;
 }
 
 module.exports = generateMarkdown;
